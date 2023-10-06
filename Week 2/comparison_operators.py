9c6487ac83399e5c07b8424d7261ffc8e3b2ce15
Kacey Laird <6lairk77@solent.ac.uk>
2023-10-06 09:43:35 +0100

    Comparison Operators

---
+++ Week 2/comparison_operators.py
@@ -0,0 +1,11 @@
+num1 = int(input("Please enter your first number: "))
+num2 = int(input("Please enter your second number: "))
+
+if num1 > num2:
+    print("Second is the smallest!")
+
+elif num2 > num1:
+    print("First is the smallest!")
+
+else:
+    print("They are both equal!")
---
+++ Week 2/modulo_operators.py
@@ -0,0 +1,7 @@
+number = int(input("Please enter a whole number: "))
+
+if (number % 2) == 0:
+    print(f"The number {number} is an even number")
+
+else:
+    print(f"The number {number} is an odd number")
