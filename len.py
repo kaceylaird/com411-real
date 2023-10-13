efccc3ee2a6c2becc705ecfb9345b49a87cce324
Kacey Laird <6lairk77@solent.ac.uk>
2023-10-13 09:57:55 +0100

    Use of summing with while loops

--- len.py
+++ len.py
@@ -3,3 +3,24 @@ wordCount = 0
 while wordCount < len(phrase):
     wordCount += 1
     print(f"{phrase}")
+
+count = 1
+total = 0
+
+print("Calculating the sum of the first 100 numbers...")
+
+while count <= 100:
+    total += count
+    count += 1
+
+print("...Done! The answer is", total)
+
+
+
+
+
+
+
+
+
+
