9af4832e66fc31e64aebdc23acf0e06c2a3aba9c
Kacey Laird <6lairk77@solent.ac.uk>
2023-10-13 09:25:58 +0100

    Use of loops with counter

--- while_loops.py
+++ while_loops.py
@@ -3,3 +3,11 @@ applesRemoved = 0
 while applesRemoved < appleCount:
     applesRemoved = applesRemoved + 1
     print("Apple Removed")
+
+obstacleCount = int(input("How many obstacles should I avoid? "))
+obstaclesAvoided = 0
+while obstaclesAvoided < obstacleCount:
+    obstaclesAvoided = obstaclesAvoided + 1
+    print(f"Avoiding done... {obstaclesAvoided} obstacle(s) avoided!")
+
+print("All obstacles avoided!")
