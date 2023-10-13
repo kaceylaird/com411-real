d2d1826e31c6294afdde30a93d711b02fa8ebadf
Kacey Laird <6lairk77@solent.ac.uk>
2023-10-13 10:26:06 +0100

    simple for loop

---
+++ Week 3/forloops.py
@@ -0,0 +1,9 @@
+mountains = int(input("How many mountains do you want to display? "))
+print("Displaying...")
+for count in range(mountains):
+    print("       __")
+    print("      /  \_")
+    print("     /^    \ ")
+    print("    /  ^    \_ ")
+    print("  _/ ^ ^     ^\ ")
+    print(" /  ^     ^    \ ")
