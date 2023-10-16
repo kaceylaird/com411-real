9227bc8a226d17545580c523f5ea42d62e2b5eeb
Kacey Laird <6lairk77@solent.ac.uk>
2023-10-16 12:03:49 +0100

    m

--- Week 3/nested_loops.py
+++ Week 3/nested_loops.py
@@ -8,3 +8,21 @@ for count in range(0, rows):
     for number in range(0, columns, 1):
         print(" :-) ", end="")
     print()
+
+sequence = input("Enter a sequence of characters consisting of dashes and two markers (e.g., 'X----X'): ")
+marker = input("Enter the marker character (e.g., 'X'): ")
+
+dash_count = 0
+
+inside_markers = False
+
+for char in sequence:
+    if char == marker:
+        if inside_markers:
+            inside_markers = False
+        else:
+            inside_markers = True
+    elif inside_markers and char == '-':
+        dash_count += 1
+
+print("Total number of characters between the markers:", dash_count)
