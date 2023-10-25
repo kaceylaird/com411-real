e54f1b7b4b98d090a35ad3649a291624f413b3ff
Kacey Laird <6lairk77@solent.ac.uk>
2023-10-25 20:12:24 +0100

    abs,range,chr

--- Week 4/built_in_functions.py
+++ Week 4/built_in_functions.py
@@ -7,3 +7,25 @@ if len(character) > 1:
 
 else:
     print(f"The ASCII code for {character} is {value}")
+
+print("Program Started!")
+
+print("Please enter an ASCII code: ")
+
+try:
+    code = int(input())
+    code = abs(code)
+except ValueError:
+    print("Error: Please enter a valid positive integer.")
+    print("Program Ended!")
+    exit()
+
+if code in range(32, 127):
+    character = chr(code)
+    print(f"The character represented by the ASCII code {code} is {character}.")
+else:
+    print("Error: The entered code is not within the printable ASCII character range (32-126).")
+
+print("Program Ended!")
+
+
