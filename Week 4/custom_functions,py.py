97056d5b6647eb3a1fbf7575fe3ddc7dff797701
Kacey Laird <6lairk77@solent.ac.uk>
2023-10-25 20:38:19 +0100

    multiple user defined parameters

--- Week 4/custom_functions,py.py
+++ Week 4/custom_functions,py.py
@@ -54,3 +54,29 @@ def climb_ladder(steps_remaining, steps_crossed):
 print("  ")
 climb_ladder(10, 5)
 climb_ladder(5, 10)
+
+print("  ")
+
+def display_ladder(steps):
+    for _ in range(steps):
+        print("| |")
+        print("***")
+
+def create_ladder():
+    try:
+        num_steps = int(input("How many steps remain?\n"))
+        if num_steps < 1:
+            print("Please enter a positive number of steps.")
+        else:
+            display_ladder(num_steps)
+    except ValueError:
+        print("Invalid input. Please enter a positive number of steps.")
+
+create_ladder()
+
+
+
+
+
+
+
