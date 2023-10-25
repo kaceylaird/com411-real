bc81ba542960cb248fbc7fbc8b79bb5ba782ad83
Kacey Laird <6lairk77@solent.ac.uk>
2023-10-25 20:44:38 +0100

    multiple function calls

--- Week 4/multiple_user_defined.py
+++ Week 4/multiple_user_defined.py
@@ -41,3 +41,64 @@ def run():
         print("Invalid choice. Please enter either 'sum' or 'average'.")
 
 run()
+
+
+# Function to display the word in a box
+def display_in_box(word):
+    box_width = len(word) + 4
+    print("*" * box_width)
+    print(f"* {word} *")
+    print("*" * box_width)
+
+
+# Function to display the word in lower-case
+def display_lower_case(word):
+    print(word.lower())
+
+
+# Function to display the word in upper-case
+def display_upper_case(word):
+    print(word.upper())
+
+
+# Function to display the word and its mirrored version
+def display_mirrored(word):
+    mirrored_word = word[::-1]
+    print(f"{word} | {mirrored_word}")
+
+
+# Function to repeat the word with alternating upper-case and lower-case
+def display_repeated(word, times):
+    repeated_word = ""
+    for i in range(times):
+        if i % 2 == 0:
+            repeated_word += word.lower()
+        else:
+            repeated_word += word.upper()
+    print(repeated_word)
+
+
+# Function to run the program
+def run():
+    word = input("Enter a word: ")
+    option = int(input(
+        "Choose an option:\n1) Display in a Box\n2) Display Lower-case\n3) Display Upper-case\n4) Display Mirrored\n5) Repeat\n"))
+
+    if option == 1:
+        display_in_box(word)
+    elif option == 2:
+        display_lower_case(word)
+    elif option == 3:
+        display_upper_case(word)
+    elif option == 4:
+        display_mirrored(word)
+    elif option == 5:
+        times = int(input("Enter how many times to display: "))
+        display_repeated(word, times)
+    else:
+        print("Invalid option. Please choose a valid option (1-5).")
+
+
+# Run the program
+run()
+
