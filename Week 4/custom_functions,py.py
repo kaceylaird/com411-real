9cf4f2161376eaa4b2b76514aa16c93fae25cf90
Kacey Laird <6lairk77@solent.ac.uk>
2023-10-25 20:27:27 +0100

    user defined using parameters

--- Week 4/custom_functions,py.py
+++ Week 4/custom_functions,py.py
@@ -14,3 +14,19 @@ def sighting():
         print("We will be fine.")
 
 sighting()
+
+def escape_by(plan):
+    if plan == "jumping over":
+        print("We cannot escape that way! The boulder is too big!")
+    elif plan == "running around":
+        print("We cannot escape that way! The boulder is moving too fast!")
+    elif plan == "cross bridge ahead":
+        print("That might just work! Let's go!")
+    else:
+        print("We cannot escape that way! The boulder is in the way!")
+
+print("  ")
+escape_by("jumping over")
+escape_by("running around")
+escape_by("cross bridge ahead")
+escape_by("digging under")
