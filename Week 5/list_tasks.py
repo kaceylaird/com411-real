c5bca24e675a70c778d9f328856746611a33fc8e
Kacey Laird <6lairk77@solent.ac.uk>
2023-10-26 12:01:11 +0100

    weak warnings

--- Week 5/list_tasks.py
+++ Week 5/list_tasks.py
@@ -2,6 +2,7 @@ def directions():
     steps = ["Move Forward", "Move Backward", "Turn Left", "Turn Right"]
     return steps
 
+
 def run_task1():
     # Call the 'directions' function
     steps = directions()
@@ -9,14 +10,16 @@ def run_task1():
     for step in steps:
         print(step)
 
+
 run_task1()
+
 
 def movements():
     path = ["Move Forward", 10, "Move Backward", 5, "Move Left", 3, "Move Right", 1]
     return path
+
+
 def generate_movements():
-    # This function generates a list of movements.
-    # In this example, I'm returning a hardcoded list for demonstration purposes.
     movements = [
         {"direction": "Forward", "steps": 10},
         {"direction": "Left", "steps": 5},
@@ -24,12 +27,14 @@ def generate_movements():
     ]
     return movements
 
+
 print(" ")
+
+
 def run_task2():
     # Display a message
     print("Moving...")
 
-    # Call the first function to get the list of movements
     movements = generate_movements()
 
     # Display the movements in the specified format
@@ -38,6 +43,6 @@ def run_task2():
         steps = move["steps"]
         print(f"{direction} for {steps} steps")
 
-# Call the second function to run the task
+
 run_task2()
 
