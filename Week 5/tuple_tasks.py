58a4dee73c9c2d5591db517df7a9a8bb4bc780f8
Kacey Laird <6lairk77@solent.ac.uk>
2023-10-27 09:49:20 +0100

    added code to demostrate a simple tuple

---
+++ Week 5/tuple_tasks.py
@@ -0,0 +1,13 @@
+def likelihood():
+    likelihoods = (50, 38, 27, 99, 4)
+    return min(likelihoods), max(likelihoods)
+
+
+def run_task1():
+    probabilities = likelihood()
+    print(f"Minimum likelihood of falling: {probabilities[0]}%")
+    print(f"Maximum likelihood of falling: {probabilities[1]}%")
+
+
+if __name__ == "__main__":
+    run_task1()
