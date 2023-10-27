eb32883c88f047349005f5f8d0af3ea033ab70e8
Kacey Laird <6lairk77@solent.ac.uk>
2023-10-27 10:00:08 +0100

    how to nest tuples and lists

--- Week 5/tuple_tasks.py
+++ Week 5/tuple_tasks.py
@@ -7,6 +7,10 @@ def run():
     print(f"Minimum likelihood of falling: {likelihood()}%")
 
 
+if __name__ == "__main__":
+    run()
+
+
 def likelihood_min_max():
     likelihoods = (50, 38, 27, 99, 4)
     return min(likelihoods), max(likelihoods)
@@ -20,3 +24,26 @@ def run_task1():
 
 if __name__ == "__main__":
     run_task1()
+
+
+def steps():
+    likelihoods = [("step 1", 50), ("step 2", 38), ("step 3", 27), ("step 4", 99), ("step 5", 4)]
+    return likelihoods
+
+
+def run():
+    all_steps = steps()
+    good_steps = []
+    bad_steps = []
+
+    for step in all_steps:
+        if step[1] >= 50:
+            bad_steps.append(step)
+        else:
+            good_steps.append(step)
+
+    print(f"Good steps: {len(good_steps)}, Bad steps: {len(bad_steps)}")
+
+
+if __name__ == "__main__":
+    run()
