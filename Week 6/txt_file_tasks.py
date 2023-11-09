1c7d51c534dda96c0bb0ffb48f1f50d31a8d7173
Kacey Laird <6lairk77@solent.ac.uk>
2023-11-09 22:10:51 +0000

    how to read a txt file

--- Week 6/txt_file_tasks.py
+++ Week 6/txt_file_tasks.py
@@ -16,3 +16,31 @@ def run():
 
 if __name__ == "__main__":
     run()
+
+
+def display_chars(file_path, num_chars):
+    with open(file_path) as file:
+        data = file.read(num_chars)
+        print(data)
+
+
+def display_line(file_path):
+    with open(file_path) as file:
+        data = file.readline().strip()
+        print(data)
+
+
+def display_text(file_path):
+    with open(file_path) as file:
+        data = file.read()
+        print(data)
+
+
+def run_task2():
+    display_chars("library.txt", 5)
+    display_line("library.txt")
+    display_text("library.txt")
+
+
+if __name__ == "__main__":
+    run_task2()
