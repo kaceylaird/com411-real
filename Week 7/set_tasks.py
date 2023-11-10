1e1ee044283dffae882723ef61ac388bf03e2150
Kacey Laird <6lairk77@solent.ac.uk>
2023-11-10 09:34:47 +0000

    how to create a set

--- Week 7/set_tasks.py
+++ Week 7/set_tasks.py
@@ -8,3 +8,28 @@ def run_task1():
 
 
 run_task1()
+
+
+def observed():
+    observations = []
+
+    for count in range(7):
+        print("Please enter an observation:")
+        observations.append(input())
+        return observations
+
+
+def run_task2():
+    print("Counting observations...")
+    observations = observed()
+
+    observations_set = set()
+    for observation in observations:
+        data = (observation, observations.count(observation))
+        observations_set.add(data)
+
+    for data in observations_set:
+        print(f"{data[0]} observed {data[1]} times.")
+
+
+run_task2()
